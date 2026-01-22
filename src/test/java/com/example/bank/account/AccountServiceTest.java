917cfee1fa0c9a56602df94d4a7c400c2832cbef
iuliana.dragoi <iulianadragoi95@gmail.com>
2026-01-22 16:20:08 +0200

    Refactor AccountServiceTest to use Mockito

--- src/test/java/com/example/bank/account/AccountServiceTest.java
+++ src/test/java/com/example/bank/account/AccountServiceTest.java
@@ -2,122 +2,156 @@ package com.example.bank.account;
 
 import com.example.bank.account.model.Account;
 import com.example.bank.account.model.AccountType;
+import com.example.bank.account.model.BusinessAccount;
+import com.example.bank.account.model.SavingsAccount;
 import com.example.bank.account.repository.AccountRepository;
-import com.example.bank.account.repository.AccountRepositoryImpl;
 import com.example.bank.account.service.AccountFactory;
-import com.example.bank.account.service.AccountIbanGenerator;
-import com.example.bank.account.service.AccountIdGenerator;
-import com.example.bank.account.service.AccountService;
-import com.example.bank.account.service.impl.AccountFactoryImpl;
 import com.example.bank.account.service.impl.AccountServiceImpl;
-import com.example.bank.account.service.impl.DefaultAccountIbanGenerator;
-import com.example.bank.bank.service.BankService;
 import com.example.bank.customer.model.Customer;
-import com.example.bank.customer.repository.CustomerRepository;
-import com.example.bank.customer.repository.CustomerRepositoryImpl;
-import com.example.bank.customer.service.CustomerIdGenerator;
-import com.example.bank.customer.service.CustomerService;
-import com.example.bank.customer.service.CustomerServiceImpl;
 import com.example.bank.transaction.repository.TransactionRepository;
-import com.example.bank.transaction.repository.TransactionRepositoryImpl;
 import com.example.bank.transaction.service.TransferService;
 import org.junit.jupiter.api.*;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class AccountServiceTest {
 
-    AccountService accountService;
-    BankService bankService;
+    @Mock
+    AccountRepository accountRepository;
+
+    @Mock
+    AccountFactory factory;
+
+    @Mock
+    TransactionRepository transactionRepository;
+
+    @Mock
+    TransferService transferService;
+
+    @InjectMocks
+    AccountServiceImpl accountService;
+
     Account account1;
     Account account2;
 
     @BeforeEach
     public void setUp() {
-        AccountRepository accountRepo = new AccountRepositoryImpl();
-        CustomerRepository customerRepo = new CustomerRepositoryImpl();
-        TransactionRepository transactionRepo = new TransactionRepositoryImpl();
-        TransferService transferService = new TransferService(accountRepo, transactionRepo);
+        account1 = new SavingsAccount(1L, "IBAN1", "Alice", 1L);
+        account2 = new SavingsAccount(2L, "IBAN2", "Bob", 2L);
 
-        AccountIbanGenerator accountIbanGenerator = new DefaultAccountIbanGenerator();
-        AccountFactory accountFactory = new AccountFactoryImpl(accountIbanGenerator);
-        CustomerService customerService = new CustomerServiceImpl(customerRepo);
+        account1.deposit(1000);
+        account2.deposit(1000);
 
-        accountService = new AccountServiceImpl(
-                accountRepo,
-                accountFactory,
-                transactionRepo,
-                transferService
-        );
-
-        bankService = new BankService(accountService, customerService);
-
-        account1 = bankService.createCustomerWithAccount("Alice", "alice@test.com", AccountType.SAVINGS);
-        account2 = bankService.createCustomerWithAccount("Bob", "bob@test.com", AccountType.SAVINGS);
-        accountService.deposit(account1.getId(), 500, "Initial deposit");
-        accountService.deposit(account2.getId(), 600, "Initial deposit");
+        lenient().when(accountRepository.findById(1L)).thenReturn(account1);
+        lenient().when(accountRepository.findById(2L)).thenReturn(account2);
+        lenient().when(accountRepository.save(any(Account.class))).thenAnswer(invocation -> invocation.getArgument(0));
     }
 
     @Test
     @Order(1)
     public void deposit() {
-        accountService.deposit(account1.getId(), 500, "Initial deposit");
-        accountService.deposit(account2.getId(), 400, "Initial deposit");
+        accountService.deposit(1L, 1000, "Deposit Alice");
+        accountService.deposit(2L, 1000, "Deposit Bob");
 
-        assertNotNull(account1);
-        assertNotNull(account2);
-        assertEquals(1000, account1.getBalance());
-        assertEquals(1000, account2.getBalance());
+        assertEquals(2000, account1.getBalance());
+        assertEquals(2000, account2.getBalance());
+
+        verify(accountRepository, times(1)).findById(1L);
+        verify(accountRepository, times(1)).findById(2L);
+        verify(accountRepository, times(2)).save(any(Account.class));
     }
 
     @Test
     @Order(2)
     public void withdraw() {
-        accountService.withdraw(account1.getId(), 200, "Initial deposit");
-        accountService.withdraw(account2.getId(), 600, "Initial deposit");
-        assertEquals(300, account1.getBalance());
-        assertEquals(0, account2.getBalance());
+        accountService.withdraw(1L, 200, "Withdraw Alice");
+        accountService.withdraw(2L, 600, "Withdraw Bob");
+
+        assertEquals(800, account1.getBalance());
+        assertEquals(400, account2.getBalance());
+
+        verify(accountRepository, times(1)).findById(1L);
+        verify(accountRepository, times(1)).findById(2L);
+        verify(accountRepository, times(2)).save(any(Account.class));
     }
 
     @Test
     @Order(3)
     public void transfer() {
-        accountService.transfer(account1.getId(), account2.getId(), 100, "Test Description");
-        assertEquals(400, account1.getBalance());
-        assertEquals(700, account2.getBalance());
+
+        doAnswer(invocation -> {
+            long fromId = invocation.getArgument(0);
+            long toId = invocation.getArgument(1);
+            double amount = invocation.getArgument(2);
+            String desc = invocation.getArgument(3);
+
+            Account from = accountRepository.findById(fromId);
+            Account to = accountRepository.findById(toId);
+
+            from.withdraw(amount);
+            to.deposit(amount);
+
+            accountRepository.save(from);
+            accountRepository.save(to);
+
+            return null;
+        }).when(transferService).transfer(anyLong(), anyLong(), anyDouble(), anyString());
+
+        accountService.transfer(1L, 2L, 300, "Alice -> Bob");
+
+        assertEquals(700, account1.getBalance());
+        assertEquals(1300, account2.getBalance());
+
+        verify(transferService, times(1)).transfer(1L, 2L, 300, "Alice -> Bob");
+        verify(accountRepository, times(1)).save(account1);
+        verify(accountRepository, times(1)).save(account2);
     }
 
     @Test
     @Order(4)
     public void openAccount() {
         Customer customer = new Customer(3, "Ana", "ana@test.com");
+        Account newAccount = new SavingsAccount(3L, "IBAN3", "Ana", customer.id());
+
+        when(factory.create(AccountType.SAVINGS, "Ana", customer.id())).thenReturn(newAccount);
+        when(accountRepository.save(any(Account.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
         Account account = accountService.openAccount(AccountType.SAVINGS, "Ana", customer.id());
-        assertNotNull(customer);
+
         assertNotNull(account);
-        assertEquals(3, account.getId());
+        assertEquals(3L, account.getId());
+        assertEquals("Ana", account.getOwnerName());
+        assertEquals(AccountType.SAVINGS, account.getType());
+
+        verify(factory, times(1)).create(AccountType.SAVINGS, "Ana", customer.id());
+        verify(accountRepository, times(1)).save(newAccount);
     }
 
     @Test
     @Order(5)
     public void findByCustomerId() {
-        List<Account> account = accountService.findByCustomerId(account1.getCustomerId());
-        assertEquals(1, account.size());
-        assertNotNull(account.get(0));
-        assertEquals(500, account.get(0).getBalance());
-    }
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new SavingsAccount(3L, "IBAN1", "Alice", 4L));
+        accounts.add(new BusinessAccount(4L, "IBAN2", "Bob", 4L, 1000));
 
-    @BeforeEach
-    public void tearUp() {
-        accountService = null;
-        bankService = null;
-        account1 = null;
-        account2 = null;
-        DefaultAccountIbanGenerator.reset();
-        CustomerIdGenerator.reset();
-        AccountIdGenerator.reset();
+        when(accountRepository.findByCustomerId(4L)).thenReturn(accounts);
+
+        List<Account> result = accountService.findByCustomerId(4L);
+
+        verify(accountRepository, times(1)).findByCustomerId(4L);
+        assertEquals(2, result.size());
+        assertNotNull(result.get(0));
+        assertNotNull(result.get(1));
     }
 }
